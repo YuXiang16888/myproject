dbe267f11ffd523df73fc5519384d4d22bc1f9a4
YushiangWang <a032165420000@gmail.com>
2022-04-13 17:42:33 +0800

    Initial commit

---
+++ src/main/java/com/xiang/Hello.java
@@ -0,0 +1,8 @@
+package com.xiang;
+
+public class Hello {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.hello();
+    }
+}
---
+++ src/main/kotlin/com/xiang/Hello.kt
@@ -0,0 +1,13 @@
+package com.xiang
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+    val human = Human()
+    human.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
